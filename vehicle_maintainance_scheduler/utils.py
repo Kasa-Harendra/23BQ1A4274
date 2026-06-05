b9e89ef4f7681b179a1814c944dbee997b0d4113
Kasa Harendra <harendrakasa@gmail.com>
2026-06-05 10:41:14 +0530

    Task 1

---
+++ vehicle_maintainance_scheduler/utils.py
@@ -0,0 +1,25 @@
+import requests
+import sys 
+import os
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(parent_dir)
+
+from logging_middleware.log import Log
+from logging_middleware.config import *
+
+def get_depots_data():
+    headers = {"Authorization": "Bearer " + ACCESS_TOKEN}
+    url = BASE_URL + "depots"
+    response = requests.get(url=url, headers=headers)
+    Log("backend", "info", "utils", "Getting depots data")
+    return response.json()
+
+def get_vehicles_data():
+    headers = {"Authorization": "Bearer " + ACCESS_TOKEN}
+    url = BASE_URL + "vehicles"
+    response = requests.get(url=url, headers=headers)
+    Log("backend", "info", "utils", "Getting vehicles data")
+    return response.json()
+
+print(get_depots_data())
